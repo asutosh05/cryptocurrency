a1cb9f792c9052c1870caf50dfea13ea26c17476
Asutosh <asutosh407@gmail.com>
2018-03-25 17:01:57 +0000

    UI page addtion

--- coins/views.py
+++ coins/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from airtable import Airtable
 import os
+from coinmarketcap import Market
+
+APIKEY='yNXRCoWSRdUIShMInMmWUVn4foIUx3'
 
 def index(request):
-    return render(request,'index.html',{})
+    coinmarketcap = Market()
+    coins=coinmarketcap.ticker(start=0, limit=3, convert='USD')
+    summery=coinmarketcap.stats(convert='USD')
+    coins_list={'coins':coins}
+    coin_summery={'summery':summery}
+    return render(request,'index.html',coin_summery)
+    
+    
