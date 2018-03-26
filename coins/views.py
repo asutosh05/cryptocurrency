891a822353f361f0414eb0ac33617caaca0c710c
asutosh-patel <asutosh4077@gmail.com>
2018-03-26 08:51:58 +0000

    basic barebone

--- coins/views.py
+++ coins/views.py
@@ -2,16 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from airtable import Airtable
 import os
-from coinmarketcap import Market
 
 APIKEY='yNXRCoWSRdUIShMInMmWUVn4foIUx3'
 
-def index(request):
-    coinmarketcap = Market()
-    coins=coinmarketcap.ticker(start=0, limit=3, convert='USD')
-    summery=coinmarketcap.stats(convert='USD')
-    coins_list={'coins':coins}
-    coin_summery={'summery':summery}
-    return render(request,'index.html',coin_summery)
+def index(request):   
+    return render(request,'index.html',{})
     
     
